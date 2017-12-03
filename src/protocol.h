9767f8267ee9ca7904248726dc28b96a6e260784
TrustyJAID <TrustyJAID@gmail.com>
2017-12-02 22:11:34 -0700

    changed rpc port

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 36327 : 36329;
+    return testnet ? 18333 : 8333;
 }
 
 
